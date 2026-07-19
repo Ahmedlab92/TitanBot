e12ba1430afff8ba68b5149d27a791f1905d525b
Ahmedlab92 <king7627lo@gmail.com>
2026-07-19 10:51:30 -0700

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,9 +23,9 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "", // required by Discord API, not shown in the client
         state: "Life is a lie",     // this is what people actually see
-        type: 5,               // Custom
+        type: 3,               // Custom
       },
     ],
   },
